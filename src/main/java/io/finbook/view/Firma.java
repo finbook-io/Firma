af97f0c5d963c7f8b333d151e17e110b3fb1316b
RaulLozanoPonce <raul.lozano.ponce@hotmail.com>
2020-02-12 13:30:20 +0000

    A simple UI has been developed. Build the main view and ask for a password

--- src/main/java/io/finbook/view/Firma.java
+++ src/main/java/io/finbook/view/Firma.java
@@ -1,8 +1,41 @@
 package io.finbook.view;
 
-public class Firma {
+import javax.swing.*;
+import java.awt.*;
+
+public class Firma extends JFrame {
+
+    private String textToSign;
+
+    public Firma(String textToSign) {
+        this.textToSign = textToSign;
+        
+        setContentPane(getPaneBuilded());
+        pack();
+        setResizable(false);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        String password = JOptionPane.showInputDialog(this, "Introduzca su contraseña de la clave privada", "Contraseña", JOptionPane.PLAIN_MESSAGE);
+        System.out.println(password);
+    }
+
+    private Container getPaneBuilded() {
+        Container pane = new JPanel();
+
+        pane.setLayout(new GridLayout(3, 2));
+        pane.add(new JLabel("Texto a firmar"));
+        pane.add(new JLabel(textToSign));
+        pane.add(new JLabel("Directorio de la Clave Privada"));
+        pane.add(new JLabel("Directorio de ejemplo"));
+        pane.add(new JLabel("Directorio del Certificado"));
+        pane.add(new JLabel("Directorio de ejemplo"));
+
+        return pane;
+    }
 
     public static void main(String[] args) {
-
+        new Firma("Texto a firmar");
     }
 }

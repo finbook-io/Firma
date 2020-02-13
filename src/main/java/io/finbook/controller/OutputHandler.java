ee60b4abb2abcc4a18fcfe792e2f3560db249f89
RaulLozanoPonce <raul.lozano.ponce@hotmail.com>
2020-02-13 11:17:52 +0000

    OutputHandler constructor has been developed. OutputHandler needs a SignData instance, to get the text signed to be able to save it in a fil

---
+++ src/main/java/io/finbook/controller/OutputHandler.java
@@ -0,0 +1,12 @@
+package io.finbook.controller;
+
+import io.finbook.model.SignData;
+
+public class OutputHandler {
+
+    private final SignData signData;
+
+    public OutputHandler(SignData signData) {
+        this.signData = signData;
+    }
+}

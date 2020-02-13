ca489f5c4412eed843400a28de8e5a92f05ca521
RaulLozanoPonce <raul.lozano.ponce@hotmail.com>
2020-02-13 10:59:02 +0000

    Signer constructor has been developed. It uses a SignerData instance to get a PrivateKey, Certificate and a text to sign

--- src/main/java/io/finbook/controller/Signer.java
+++ src/main/java/io/finbook/controller/Signer.java
@@ -1,5 +1,14 @@
 package io.finbook.controller;
 
+import io.finbook.model.SignData;
+
 public class Signer {
 
+    private final SignData signData;
+
+    public Signer(SignData signData) {
+        this.signData = signData;
+    }
+
+    
 }

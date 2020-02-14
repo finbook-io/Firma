750996bd4a07a9d782b0ef7bbaad493060481906
RaulLozanoPonce <raul.lozano.ponce@hotmail.com>
2020-02-14 12:51:22 +0000

    FirmaSwing view has been refactored and abstracted using interfaces. All view classes will implement UserInterface class, and all Firma classes will implemeht Firma class

--- src/main/java/io/finbook/view/Firma.java
+++ src/main/java/io/finbook/view/Firma.java
@@ -1,125 +1,29 @@
 package io.finbook.view;
 
-import io.finbook.controller.*;
-import io.finbook.model.FirmaData;
-import io.finbook.model.SignData;
+public interface Firma {
 
-import javax.swing.*;
-import java.awt.*;
-import java.io.FileNotFoundException;
+    void init();
+    void cleanOutputFile();
+    void setCertificatePath();
+    void setPrivateKeyPath();
+    void getCertificate();
+    void getPrivateKey();
+    void signText();
+    void saveTextSigned();
+    void showMessage(String title, String message, int messageType);
 
-public class Firma extends JFrame {
-
-    private String textToSign;
-    private FirmaData firmaData;
-    private SignData signData;
-
-    public Firma(String textToSign) {
-        this.textToSign = textToSign;
-        this.firmaData = new FirmaData();
-        this.signData = new SignData(textToSign);
-
-        cleanOutputFile();
-        setPaths();
-        interfaceDisplay();
-        getFiles();
-        signAndSave();
-
-        showMessage("Firma completada", "La firma ha sido completada correctamente", JOptionPane.INFORMATION_MESSAGE);
-    }
-
-    private void cleanOutputFile() {
-        new OutputHandler(signData).deleteContentOfOutputFile();
-    }
-
-    private void setPaths() {
+    default void setPaths() {
         setCertificatePath();
         setPrivateKeyPath();
     }
 
-    private void setCertificatePath() {
-        new CertificateHandler(firmaData, signData).setCertificatePath();
-    }
-
-    private void setPrivateKeyPath() {
-        new PrivateKeyHandler(firmaData, signData, signData.getCertificate()).setPrivateKeyPath();
-    }
-
-    private void interfaceDisplay() {
-        updateContentPane();
-        setUpWindow();
-    }
-
-    private void updateContentPane() {
-        Container pane = new JPanel();
-
-        pane.setLayout(new GridLayout(3, 2));
-        pane.add(new JLabel("Texto a firmar"));
-        pane.add(new JLabel(textToSign));
-        pane.add(new JLabel("Directorio de la Clave Privada"));
-        pane.add(new JLabel(firmaData.getPrivateKeyPath()));
-        pane.add(new JLabel("Directorio del Certificado"));
-        pane.add(new JLabel(firmaData.getCertificatePath()));
-
-        setContentPane(pane);
-    }
-
-    private void setUpWindow() {
-        pack();
-        setResizable(false);
-        setVisible(true);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-
-    private void getFiles() {
+    default void getFiles() {
         getCertificate();
         getPrivateKey();
     }
 
-    private void getCertificate() {
-        try {
-            new CertificateHandler(firmaData, signData).getCertificate();
-        } catch (FirmaHandler.InvalidCertificate invalidCertificate) {
-            showMessage("Certificado no válido", "Ha ocurrido algún error al cargar el certificado", JOptionPane.ERROR_MESSAGE);
-        } catch (FileNotFoundException e) {
-            showMessage("Certificado no encontrado", "El certificado no ha sido encontrado, o no ha sido especificado", JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-    private void getPrivateKey() {
-        String password = JOptionPane.showInputDialog(this, "Introduzca su contraseña de la clave privada", "Contraseña", JOptionPane.PLAIN_MESSAGE);
-
-        try {
-            new PrivateKeyHandler(firmaData, signData, signData.getCertificate()).getPrivateKey(password);
-        } catch (FirmaHandler.InvalidPassword invalidPassword) {
-            showMessage("Contraseña no válida", "La contraseña introducida no corresponde con la de la clave privada", JOptionPane.ERROR_MESSAGE);
-        } catch (FirmaHandler.InvalidCertificate invalidCertificate) {
-            showMessage("Certificado no válido", "El certificado no corresponde al asociado con la clave privada", JOptionPane.ERROR_MESSAGE);
-        } catch (FileNotFoundException e) {
-            showMessage("Clave privada no encontrada", "La clave privada no ha sido encontrada, o no ha sido especificada", JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-    private void signAndSave() {
+    default void signAndSave() {
         signText();
         saveTextSigned();
-    }
-
-    private void signText() {
-        new Signer(signData).sign();
-    }
-
-    private void saveTextSigned() {
-        new OutputHandler(signData).saveText();
-    }
-
-    private void showMessage(String title, String message, int messageType) {
-        JOptionPane.showMessageDialog(this, message, title, messageType);
-        System.exit(0);
-    }
-
-    public static void main(String[] args) {
-        new Firma("Texto de ejemplo");
     }
 }
---
+++ src/main/java/io/finbook/view/UserInterface.java
@@ -0,0 +1,12 @@
+package io.finbook.view;
+
+public interface UserInterface {
+    default void interfaceDisplay() {
+        updatePanel();
+        setUpWindow();
+    }
+
+    void updatePanel();
+
+    void setUpWindow();
+}
